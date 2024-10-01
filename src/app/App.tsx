279b67d058eed25efd6dfa5af5477af6539394bf
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-01 15:19:05 +0330

    Header

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Header } from "../components/common/Header/HolderHeader/Header.tsx"
+
+function App() {
+
+  return (
+    <>
+      <div> 
+        {/* Routers */}
+          <Header/>
+      </div>
+    </>
+  )
+}
+
+export default App 
