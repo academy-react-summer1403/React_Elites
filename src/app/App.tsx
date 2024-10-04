fcedeeb7823177435425695ed8dfa7ad0de86264
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-04 11:34:14 +0330

    Login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,17 @@ import React from "react"
 import { Header } from "../components/common/Header/HolderHeader/Header.tsx"
 import { HolderFooter } from "../components/common/Footer/HolderFooter/HolderFooter.tsx"
 import { LandingHolder } from "../components/Landing/A-LandingHolder/LandingHolder.tsx"
+import { Login } from "../components/login/login.tsx"
 
 function App() {
 
   return (
     <>
         {/* Routers */}
-          <Header/>
+          {/* <Header/>
           <LandingHolder />
-          <HolderFooter/>
+          <HolderFooter/> */}
+          <Login />
     </>
   )
 }
