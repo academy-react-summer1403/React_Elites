b277cf8e265242330085808ab1aca1005f3c0c11
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-05 21:03:51 +0330

    Bug Fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,7 @@ function App() {
           {/* <Header/>
           <LandingHolder />
           <HolderFooter/> */}
-          {/* <Login /> */}
+        {/* <Login /> */}
           {/* <ForgetPass /> */}
     </>
   )
