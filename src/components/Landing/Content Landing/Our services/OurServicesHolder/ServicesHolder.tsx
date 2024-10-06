99974be66add60b599d67f93796a00c0c432b82d
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-06 20:15:21 +0330

    LANDING Bug FIX

--- src/components/Landing/Content Landing/Our services/OurServicesHolder/ServicesHolder.tsx
+++ src/components/Landing/Content Landing/Our services/OurServicesHolder/ServicesHolder.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import style from './Holder.module.css'
 import { ServicesItemsHolder } from '../OurServicesItems/ServicesItemsHolder/ServicesItemsHolder'
 import { ServicesTitle } from '../OurServicesItems/ServicesItemsHolder/ServicesTitle'
+import azmoon from '../../../../../assets/Images/azmoon.png'
+import work from '../../../../../assets/Images/work.png'
+import moshavere from '../../../../../assets/Images/moshavere.png'
+import madrak from '../../../../../assets/Images/madrak.png'
+
 const ServicesHolder = () => {
   let data = [
-    {title: "آزمون ها", desc: "با آزمون های تعیین سطح شما میتوانید سطح دانش خودتون رو بسنجید", image: ""},
-    {title: "مدرک معتبر", desc: "با مدرک ما میتوانید به راحتی در همه جا استخدام بشید", image: ""},
-    {title: "فرصت‌های شغلی", desc: "با توجه به سطح توانایی شما فرصت های شغلی به شما پیشنهاد داده میشه", image: ""},
-    {title: "مشاوره 24 ساعته", desc: "مشاورین ما 24 ساعته جوابگو سوال های شما هستند", image: ""}
+    {title: "آزمون ها", desc: "با آزمون های تعیین سطح شما میتوانید سطح دانش خودتون رو بسنجید", image: azmoon},
+    {title: "مدرک معتبر", desc: "با مدرک ما میتوانید به راحتی در همه جا استخدام بشید", image: madrak},
+    {title: "فرصت‌های شغلی", desc: "با توجه به سطح توانایی شما فرصت های شغلی به شما پیشنهاد داده میشه", image: work},
+    {title: "مشاوره 24 ساعته", desc: "مشاورین ما 24 ساعته جوابگو سوال های شما هستند", image: moshavere}
   ]
   return (
     <div className={style.holder}>
