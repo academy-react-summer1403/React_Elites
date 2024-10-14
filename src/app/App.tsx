b3837de08ced326c88002f1dedf1b075527cbdac
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-14 15:29:34 +0330

    Landing Bug Fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,8 +15,8 @@ function App() {
       <Header/>
 
       {/* <BlogsList /> */}
-      {/* <LandingHolder /> */}
-      <Login />
+      <LandingHolder />
+      {/* <Login /> */}
       {/* <ForgetPass /> */}
 
       <HolderFooter/>
