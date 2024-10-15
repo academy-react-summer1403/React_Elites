fd581e214d2b44eeed80d402e130f8a8bde081a9
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-15 21:40:46 +0330

    Add Error Page And 404

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,20 +6,23 @@ import { Login } from "../components/login/login.tsx"
 import { ForgetPass } from "../components/forgetPassword/forgetPassword.tsx"
 import { Blogs } from "../components/Landing/Content Landing/Blogs/BlogsHolder/Blogs.tsx"
 import { BlogsList } from "../components/blogs/blogsList.tsx"
+import { ErrorPage } from "../components/Error Page/ErrorPage.tsx"
+import { ErrorPage404 } from "../components/Error 404/Error404.tsx"
 
 function App() {
 
   return (
     <>
       {/* Routers */}
-      <Header/>
+      {/* <Header/> */}
 
       {/* <BlogsList /> */}
       {/* <LandingHolder /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <ForgetPass /> */}
-
-      <HolderFooter/>
+      <ErrorPage />
+      <ErrorPage404 />
+      {/* <HolderFooter/> */}
     </>
   )
 }
