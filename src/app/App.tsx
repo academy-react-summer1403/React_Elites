af5b0af5031e22f4e87685ef8949c57b5c9836fe
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-17 19:44:40 +0330

    conflict resolve

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,10 @@
 import React from "react"
-import { Header } from "../components/common/Header/HolderHeader/Header.tsx"
-import { HolderFooter } from "../components/common/Footer/HolderFooter/HolderFooter.tsx"
-import { LandingHolder } from "../components/Landing/A-LandingHolder/LandingHolder.tsx"
-import { Login } from "../components/login/login.tsx"
-import { ForgetPass } from "../components/forgetPassword/forgetPassword.tsx"
-import { Blogs } from "../components/Landing/Content Landing/Blogs/BlogsHolder/Blogs.tsx"
-import { BlogsList } from "../components/blogs/blogsList.tsx"
-import { ErrorPage } from "../components/Error Page/ErrorPage.tsx"
-import { ErrorPage404 } from "../components/Error 404/Error404.tsx"
 
 function App() {
 
   return (
     <>
-      {/* Routers */}
-      {/* <Header/> */}
 
-      {/* <BlogsList /> */}
-      {/* <LandingHolder /> */}
-      {/* <Login /> */}
-      {/* <ForgetPass /> */}
-      <ErrorPage />
-      <ErrorPage404 />
-      {/* <HolderFooter/> */}
     </>
   )
 }
