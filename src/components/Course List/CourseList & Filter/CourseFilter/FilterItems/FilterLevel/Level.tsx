4b5aeba94f4268be76d86620fa559bca4a2c316a
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-21 17:38:11 +0330

    Course List Responsive

--- src/components/Course List/CourseList & Filter/CourseFilter/FilterItems/FilterLevel/Level.tsx
+++ src/components/Course List/CourseList & Filter/CourseFilter/FilterItems/FilterLevel/Level.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './Level.module.css'
+import { useGlobalState } from '../../../../../../State/State';
 
 const Level = () => {
+  const [darkMode, setDarkMode] = useGlobalState('DarkMode');
   return (
     <>
-        <div className={style.titleLevel} > سطح آموزشی </div>
-        <div className={style.level}>سطح مورد نظر را انتخاب کنید</div>
+        <div className={style.titleLevel} data-theme={darkMode ? "darkNoBG" : "lightMode"}> سطح آموزشی </div>
+        <div className={style.level} data-theme={darkMode ? "dark" : "lightMode"}>سطح مورد نظر را انتخاب کنید</div>
     </>
   )
 }
--- src/components/Course List/CourseListHolder/CourseList.tsx
+++ src/components/Course List/CourseListHolder/CourseList.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import style from './CourseList.module.css'
 import { StartNewJourney } from '../Start New Journy/StartNewJourneyHolder/StartNewJourney'
 import { CourseListAndFilter } from '../CourseList & Filter/CourseList&FilterHolder/CourseListAndFilter'
+import { useGlobalState } from '../../../State/State'
 
 const CourseList = () => {
+  const [darkMode, setDarkMode] = useGlobalState('DarkMode');
   return (
-    <>
+    <div className={style.body}  data-theme={darkMode ? "dark" : "lightMode"}>
         <div className={style.courseListHolder}>
         <StartNewJourney />
         <CourseListAndFilter />
         </div>
-    </>
+    </div>
   )
 }
 
