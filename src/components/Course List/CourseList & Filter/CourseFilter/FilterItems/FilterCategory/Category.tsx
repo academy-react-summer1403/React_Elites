2663ea3059511ec2cedd20db4c8c44c6dad0215b
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-22 21:14:12 +0330

    bug fix

--- src/components/Course List/CourseList & Filter/CourseFilter/FilterItems/FilterCategory/Category.tsx
+++ src/components/Course List/CourseList & Filter/CourseFilter/FilterItems/FilterCategory/Category.tsx
@@ -6,7 +6,7 @@ const Category = () => {
   const [darkMode, setDarkMode] = useGlobalState('DarkMode');
   return (
     <>
-        <div className={style.titleCat} data-theme={darkMode ? "darkNoBG" : "lightMode"}> دسته بندی </div>
+        <div className={style.titleCat} data-theme={darkMode ? "cat" : "lightMode"}> دسته بندی </div>
         <div className={style.cat} data-theme={darkMode ? "dark" : "lightMode"}>دسته مورد نظر را انتخاب کنید</div>
     </>
   )
