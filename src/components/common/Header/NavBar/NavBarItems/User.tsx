9d47c1780ba6654fd29aa56ccdc3386df3cf9ef5
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-10-29 14:28:37 +0330

    Bug Fix

--- src/components/common/Header/NavBar/NavBarItems/User.tsx
+++ src/components/common/Header/NavBar/NavBarItems/User.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import style from './User.module.css'
 import { useGlobalState } from '../../../../../State/State'
+import { NavLink } from 'react-router-dom';
 
 const User = () => {
   const [userInfoObj, setUserInfoObj] = useGlobalState('userInfoObj');
@@ -15,34 +16,34 @@ const User = () => {
       {isClicked && <div className={style.userAccebility}>
         <div className={style.fixed}>
           <div className={style.holder}>
-            <div className={style.items}> داشبورد </div>
+            <NavLink to="/" className={style.items}> داشبورد </NavLink>
           </div>
           <div className={style.holder}>
-            <div className={style.items}> دوره من </div>
-          </div>
-
-          <div className={style.holder}>
-            <div className={style.items}> رزرو من </div>
+            <NavLink to="Student-Panel/My-Courses" className={style.items}> دوره من </NavLink>
           </div>
 
           <div className={style.holder}>
-            <div className={style.items}> دوره های مورد علاقه </div>
+            <NavLink to="Student-Panel/My-Reserve" className={style.items}> رزرو من </NavLink>
           </div>
 
           <div className={style.holder}>
-            <div className={style.items}> بلاگ های مورد علاقه </div>
+            <NavLink to="Student-Panel/Fav-Courses" className={style.items}> دوره های مورد علاقه </NavLink>
+          </div>
+
+          <div className={style.holder}>
+            <NavLink to="/" className={style.items}> بلاگ های مورد علاقه </NavLink>
           </div>
   
           <div className={style.holder}>
-            <div className={style.items}> پروفایل </div>
+            <NavLink to="Student-Panel/Information" className={style.items}> پروفایل </NavLink>
           </div>
 
           <div className={style.holder}>
-            <div className={style.items}> پرداخت ها </div>
+            <NavLink to="/Student-Panel/Shopping-Basket" className={style.items}> پرداخت ها </NavLink>
           </div>
 
           <div className={style.holder}>
-            <div className={style.items}> خروج </div>
+            <NavLink to="/" className={style.items}> خروج </NavLink>
           </div>
 
         </div>
