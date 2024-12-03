b46f163b043fb4e7294668e42665fcb44c7e1dd6
taha <taha2010talebi@gmail.com>
2024-12-03 11:30:27 +0330

    PaymentGateway

---
+++ src/components/Payment Gateway/Footer/Items/FooterFixed.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Items.module.css'
+
+const FooterFixed = () => {
+  return (
+    <div className={style.footerFixed}>
+      <div>
+        <h1>شماره تماس: 27312731-021 </h1>
+        <h1>شرکت به پرداخت ملت ارايه دهنده خدمات نوین پرداخت الکترونيک</h1>
+      </div>
+    </div>
+  )
+}
+
+export { FooterFixed }
