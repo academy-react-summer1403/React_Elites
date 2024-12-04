3697e804d44f42666e01a48dfbbd48a11cb15493
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-12-04 10:18:14 +0330

    Factor

---
+++ src/components/Payment First Page/Payment Holder/Factor.tsx
@@ -0,0 +1,15 @@
+// import { usePDF } from 'react-to-pdf';
+
+// const Factor = () => {
+//    const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
+//    return (
+//       <div>
+//          <button onClick={() => toPDF()} className='DannaM'> مشاهده رسید </button>
+//          <div ref={targetRef}>
+//             Content to be generated to PDF
+//          </div>
+//       </div>
+//    )
+// }
+
+// export {Factor}
