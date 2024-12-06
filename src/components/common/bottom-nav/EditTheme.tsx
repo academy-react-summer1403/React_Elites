0d53728162e737bb200e269315a6a170f91afc4b
taha <taha2010talebi@gmail.com>
2024-12-06 18:07:36 +0330

    s

--- src/components/common/bottom-nav/EditTheme.tsx
+++ src/components/common/bottom-nav/EditTheme.tsx
@@ -6,15 +6,19 @@ import { useGlobalState } from "../../../State/State";
     const [isClicked, setisClicked] = useState(false)
     const [darkMode, setDarkMode] = useGlobalState('DarkMode');
     const [darkMode2, setDarkMode2] = useGlobalState('DarkMode2');
-    const [GreenMode, setGreenMode] = useGlobalState('GreenMode');
+    const [greenMode, setGreenMode] = useGlobalState('GreenMode');
+    const [pinkMode, setPinkMode] = useGlobalState('PinkMode');
+    const [blueMode, setBlueMode] = useGlobalState('BlueMode');
+    const [redMode, setRedMode] = useGlobalState('RedMode');
+    
 
   return (
     <div className={style.pencil} onClick={() => setisClicked(!isClicked)}>
         {isClicked && <div className={style.holderPalette}>
-            <div className={style.theme1}></div>
-            <div className={style.theme2} onClick={() => setGreenMode(!GreenMode)}></div>
-            <div className={style.theme3}></div>
-            <div className={style.theme4}></div>
+            <div className={style.theme1} onClick={() => setPinkMode(!pinkMode)}></div>
+            <div className={style.theme2} onClick={() => setGreenMode(!greenMode)}></div>
+            <div className={style.theme3} onClick={() => setBlueMode(!blueMode)}></div>
+            <div className={style.theme4} onClick={() => setRedMode(!redMode)}></div>
             <div className={style.theme5} onClick={() => setDarkMode2(!darkMode2)}></div>
             <div className={style.theme6} onClick={() => setDarkMode(!darkMode)}></div>
         </div>}
