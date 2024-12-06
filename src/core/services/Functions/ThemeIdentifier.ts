1eadd4f17a969b6ba771a5d4e368b404283a4928
taha <taha2010talebi@gmail.com>
2024-12-06 17:51:10 +0330

    s

--- src/core/services/Functions/ThemeIdentifier.ts
+++ src/core/services/Functions/ThemeIdentifier.ts
@@ -2,16 +2,16 @@ import { useGlobalState } from "../../../State/State";
 
 
 
-export const identifier = (className, className2, className3, className4, className5, className6,) => {
+export const identifier = (className, className2, className3, className4, className5, className6) => {
 
 
     // اضافه کردن تم ها
     const [darkMode, setDarkMode] = useGlobalState('DarkMode');
     const [darkMode2, setDarkMode2] = useGlobalState('DarkMode2');
     const [greenMode, setGreenMode] = useGlobalState('GreenMode');
-    const [pinkMode2, setPinkMode2] = useGlobalState('PinkMode');
+    const [pinkMode, setPinkMode] = useGlobalState('PinkMode');
     const [blueMode, setBlueMode] = useGlobalState('BlueMode');
-    const [redMode2, setRedMode2] = useGlobalState('RedMode');
+    const [redMode, setRedMode] = useGlobalState('RedMode');
 
     // اگر این تم روشن شودس
 
@@ -24,13 +24,13 @@ export const identifier = (className, className2, className3, className4, classN
     else if (greenMode == true) {
         return className3
     }
-    else if (pinkMode2 == true) {
+    else if (pinkMode == true) {
         return className4
     }
     else if (blueMode == true) {
         return className5
     }
-    else if (redMode2 == true) {
+    else if (redMode == true) {
         return className6
     }
 }
