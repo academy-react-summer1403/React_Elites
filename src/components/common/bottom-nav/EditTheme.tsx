6ecc2d0c751a293aa1af428fc3eda0ba258adcda
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-12-06 18:16:49 +0330

    Change Theme

--- src/components/common/bottom-nav/EditTheme.tsx
+++ src/components/common/bottom-nav/EditTheme.tsx
@@ -15,12 +15,54 @@ import { useGlobalState } from "../../../State/State";
   return (
     <div className={style.pencil} onClick={() => setisClicked(!isClicked)}>
         {isClicked && <div className={style.holderPalette}>
-            <div className={style.theme1} onClick={() => setPinkMode(!pinkMode)}></div>
-            <div className={style.theme2} onClick={() => setGreenMode(!greenMode)}></div>
-            <div className={style.theme3} onClick={() => setBlueMode(!blueMode)}></div>
-            <div className={style.theme4} onClick={() => setRedMode(!redMode)}></div>
-            <div className={style.theme5} onClick={() => setDarkMode2(!darkMode2)}></div>
-            <div className={style.theme6} onClick={() => setDarkMode(!darkMode)}></div>
+            <div className={style.theme1} onClick={() => {
+              setDarkMode(false)
+              setDarkMode2(false)
+              setRedMode(!redMode)
+              setBlueMode(false)
+              setGreenMode(false)
+              setPinkMode(false)
+            }}></div>
+            <div className={style.theme2} onClick={() => {
+              setDarkMode(false)
+              setDarkMode2(false)
+              setRedMode(false)
+              setBlueMode(false)
+              setGreenMode(!greenMode)
+              setPinkMode(false)
+            }}></div>
+            <div className={style.theme3} onClick={() => {
+              setDarkMode(false)
+              setDarkMode2(!darkMode2)
+              setRedMode(false)
+              setBlueMode(false)
+              setGreenMode(false)
+              setPinkMode(false)
+            }}></div>
+            <div className={style.theme4} onClick={() => {
+              setDarkMode(false)
+              setDarkMode2(false)
+              setRedMode(false)
+              setBlueMode(!blueMode)
+              setGreenMode(false)
+              setPinkMode(false)
+            }}></div>
+            <div className={style.theme5} onClick={() => {
+              setDarkMode(false)
+              setDarkMode2(false)
+              setRedMode(false)
+              setBlueMode(false)
+              setGreenMode(false)
+              setPinkMode(!pinkMode)
+            }}></div>
+            <div className={style.theme6} onClick={() => {
+              setDarkMode(!darkMode)
+              setDarkMode2(false)
+              setRedMode(false)
+              setBlueMode(false)
+              setGreenMode(false)
+              setPinkMode(false)
+            }}></div>
         </div>}
     </div>
   )
